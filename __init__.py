5b0754eaa1ea17c671a48a5268d7e01b9a15abb9
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-11 18:07:21 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,15 +8,26 @@ __version__ = "0.0.1"
 __doc__ = "Simple plugin for mwdb-core that requests a file report from VirusTotal"
 
 
-logger = logging.getLogger("mwdb.plugin.vt")
+logger = logging.getLogger("mwdb.plugin.virustotal")
+
+
+config_api_key = ""
+
+
+def VtProcessFile(file: File):
+    file.add_comment(f"VirusTotal results:")
+    
+    pass
 
 
 class VtHookHandler(PluginHookHandler):
     def on_created_file(self, file: File):
         logger.info("on_created_file() - File report requested", file.file_name)
+        VtProcessFile(file)
 
     def on_reuploaded_file(self, file: File):
         logger.info("on_reuploaded_file() - File report requested", file.file_name)
+        VtProcessFile(file)
 
 
 def entrypoint(app_context: PluginAppContext):
@@ -26,7 +37,7 @@ def entrypoint(app_context: PluginAppContext):
     This will be called on app load.
     """
     app_context.register_hook_handler(VtHookHandler)
-    logger.info("Plugin hook handler registered.")
+    logger.info("Plugin hook handler is registered.")
 
 
 __plugin_entrypoint__ = entrypoint
