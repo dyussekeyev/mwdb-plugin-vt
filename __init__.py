eee21b86da874ee0d5f2251564430f51b763c9c5
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-12 11:34:35 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+from datetime import datetime
 
 from mwdb.core.plugins import PluginAppContext, PluginHookHandler
 from mwdb.model import File
@@ -14,24 +16,58 @@ logger = logging.getLogger("mwdb.plugin.virustotal")
 
 config_api_url = ""
 config_api_key = ""
+config_vt_api_key = ""
 
 
 def VtProcessFile(sha256):
     mwdb = MWDB(api_url=config_api_url, api_key=config_api_key)
     
     file = mwdb.query_file(sha256)
-    file.add_comment("VirusTotal results:")
+       
+    response = requests.get(f"https://www.virustotal.com/api/v3/files/{sha256}", headers={"x-apikey": config_vt_api_key})
+    
+    if response.status_code == 200:
+        data = response.json()["data"]
+        attributes = data["attributes"]
+        
+        comment = f"""
+        VT Link: {data['links']['self']}
+        
+        malicious: {attributes['last_analysis_stats']['malicious']}
+        suspicious: {attributes['last_analysis_stats']['suspicious']}
+        undetected: {attributes['last_analysis_stats']['undetected']}
+        harmless: {attributes['last_analysis_stats']['harmless']}
+        
+        first_submission_date: {datetime.utcfromtimestamp(attributes['first_submission_date']).strftime('%Y-%m-%d %H:%M:%S UTC')}
+        last_submission_date: {datetime.utcfromtimestamp(attributes['last_submission_date']).strftime('%Y-%m-%d %H:%M:%S UTC')}
+        last_analysis_date: {datetime.utcfromtimestamp(attributes['last_analysis_date']).strftime('%Y-%m-%d %H:%M:%S UTC')}
+        """
 
-    pass
+        for engine, result in attributes['last_analysis_results'].items():
+            if result['category'] in ['malicious', 'suspicious']:
+                comment += f"{engine}: {result['result']}\n"
+
+        comment += f"""
+        unique_sources: {attributes['unique_sources']}
+        reputation: {attributes['reputation']}
+        times_submitted: {attributes['times_submitted']}
+        """
+        
+        file.add_comment(comment.strip())
+    else:
+        error = response.json().get("error", {})
+        code = error.get("code", "Unknown")
+        message = error.get("message", "No message provided")
+        logger.info("VirusTotal error: %s %s for sha256 = %s", code, message, sha256)
 
 
 class VtHookHandler(PluginHookHandler):
     def on_created_file(self, file: File):
-        logger.info("on_created_file() - File report requested (sha256 = %s)", file.sha256)
+        logger.info("on_created_file(): File report requested for sha256 = %s", file.sha256)
         VtProcessFile(file.sha256)
 
     def on_reuploaded_file(self, file: File):
-        logger.info("on_reuploaded_file() - File report requested (sha256 = %s)", file.sha256)
+        logger.info("on_reuploaded_file(): File report requested for sha256 = %s", file.sha256)
         VtProcessFile(file.sha256)
 
 
